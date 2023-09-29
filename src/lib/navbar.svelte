6c0776cd363a4ccf7eed390d9714d45b2ad36c4c
Hayat Tamboli <hayat.tamboli@gmail.com>
2023-09-30 00:41:38 +0530

    added navbar component

---
+++ src/lib/navbar.svelte
@@ -0,0 +1,42 @@
+<nav class="w-full flex justify-center align-middle">
+  <div class="inline-flex mx-auto">
+    <div class="relative flex h-16 items-center justify-between bg-gray-200 px-4 mt-4 rounded-lg">
+      <div
+        class="flex flex-1 items-center justify-center sm:items-stretch "
+      >
+
+          <div class="flex space-x-4">
+            <a
+              href="#"
+              class="bg-gray-300 text-gray-800 rounded-md px-3 py-2 text-sm font-medium"
+              aria-current="page">Home</a
+            >
+            <a
+              href="#"
+              class="text-gray-800 hover:bg-gray-300  rounded-md px-3 py-2 text-sm font-medium"
+              >Team</a
+            >
+            <a
+              href="#"
+              class="text-gray-800 hover:bg-gray-300  rounded-md px-3 py-2 text-sm font-medium"
+              >Issues for mangrove</a
+            >
+            <a
+              href="#"
+              class="text-gray-800 hover:bg-gray-300  rounded-md px-3 py-2 text-sm font-medium"
+              >TBD</a
+            >
+          </div>
+      
+      </div>
+    </div>
+  </div>
+
+</nav>
+
+<style lang="postcss">
+  :global(html) {
+    background-color: theme(colors.gray.100);
+  }
+</style>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,10 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<script>
+  import Navbar from '../lib/navbar.svelte';
+</script>
+
+<Navbar></Navbar>
+
+<h1 class="text-3xl font-bold underline">Let's go mangrove</h1>
 
 <style lang="postcss">
   :global(html) {
